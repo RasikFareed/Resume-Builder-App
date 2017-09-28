85136fbcca50da3380c84a389f5db7796b46bbe5
Nesh-Rasikfareed <rasik.fareed@neshinc.com>
2017-09-28 11:50:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/rasik/resumebuilder/Adapter/TabPageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.rasik.resumebuilder.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.rasik.resumebuilder.Fragments.EducationFragment;
+import com.example.rasik.resumebuilder.Fragments.InterestFragment;
+import com.example.rasik.resumebuilder.Fragments.PersonalFragment;
+
+/**
+ * Created by rasik on 28/9/17.
+ */
+
+public class TabPageAdapter extends FragmentPagerAdapter {
+
+    public TabPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                return new PersonalFragment();
+            case 1:
+                return new EducationFragment();
+            case 2:
+                return new InterestFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 3;
+    }
+
+}
---
+++ app/src/main/java/com/example/rasik/resumebuilder/Fragments/EducationFragment.java
@@ -0,0 +1,24 @@
+package com.example.rasik.resumebuilder.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rasik.resumebuilder.R;
+
+/**
+ * Created by rasik on 28/9/17.
+ */
+
+public class EducationFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.education_fragment, container, false);
+
+        return rootView;
+    }
+}
