fffd735a957237f7eb0e0a0c8ed3b2a912295376
RasikFareed <muhammed.rasik.795@gmail.com>
2017-09-28 17:58:40 +0530

    Added Icon

--- app/src/main/java/com/example/rasik/resumebuilder/Fragments/EducationFragment.java
+++ app/src/main/java/com/example/rasik/resumebuilder/Fragments/EducationFragment.java
@@ -2,9 +2,12 @@ package com.example.rasik.resumebuilder.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.rasik.resumebuilder.R;
 
@@ -13,11 +16,39 @@ import com.example.rasik.resumebuilder.R;
  */
 
 public class EducationFragment extends Fragment {
+
+    EditText inputTenthSchoolName,inputTenthYear,inputTenthPercentage,inputTwelthSchoolName,inputTwelthYear,inputTwelthPercentage,inputCollegeName,
+            inputCollegeYear,inputCollegePercentage;
+    Button btn_next2;
+    ViewPager viewPager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.education_fragment, container, false);
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+
+        inputTenthSchoolName = (EditText)rootView.findViewById(R.id.inputTenthSchoolName);
+        inputTenthYear = (EditText)rootView.findViewById(R.id.inputTenthYear);
+        inputTenthPercentage = (EditText)rootView.findViewById(R.id.inputTenthPercentage);
+
+        inputTwelthSchoolName = (EditText)rootView.findViewById(R.id.inputTwelthSchoolName);
+        inputTwelthYear = (EditText)rootView.findViewById(R.id.inputTwelthYear);
+        inputTwelthPercentage = (EditText)rootView.findViewById(R.id.inputTwelthPercentage);
+
+        inputCollegeName = (EditText)rootView.findViewById(R.id.inputCollegeName);
+        inputCollegeYear = (EditText)rootView.findViewById(R.id.inputCollegeYear);
+        inputCollegePercentage = (EditText)rootView.findViewById(R.id.inputCollegePercentage);
+
+        btn_next2 = (Button)rootView.findViewById(R.id.btn_next2);
+        btn_next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPager = getActivity().findViewById(R.id.viewpager);
+                viewPager.setCurrentItem(2);
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/com/example/rasik/resumebuilder/Fragments/PersonalFragment.java
+++ app/src/main/java/com/example/rasik/resumebuilder/Fragments/PersonalFragment.java
@@ -3,10 +3,13 @@ package com.example.rasik.resumebuilder.Fragments;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -21,14 +24,28 @@ import java.util.Calendar;
 
 public class PersonalFragment extends Fragment {
 
-    EditText input_date;
+    EditText input_date,inputFirstName,inputLastName,input_email,input_street,input_district,input_pincode;
+    Button next1;
+    ViewPager viewPager;
+    TabLayout tabLayout;
     Calendar myCalendar = Calendar.getInstance();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.personal_info_fragment, container, false);
+
+        inputFirstName = (EditText)rootView.findViewById(R.id.inputFirstName);
+        inputLastName = (EditText) rootView.findViewById(R.id.inputLastName);
+        input_email = (EditText)rootView.findViewById(R.id.input_email);
         input_date = (EditText)rootView.findViewById(R.id.input_date);
+        input_street = (EditText)rootView.findViewById(R.id.input_street);
+        input_district = (EditText)rootView.findViewById(R.id.input_district);
+        input_pincode = (EditText)rootView.findViewById(R.id.input_pincode);
+
+        next1 = (Button)rootView.findViewById(R.id.btn_next);
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+
         input_date.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -37,9 +54,17 @@ public class PersonalFragment extends Fragment {
                 DatePickerFragment dpf = new DatePickerFragment().newInstance();
                 dpf.setCallBack(onDate);
                 dpf.show(getFragmentManager().beginTransaction(), "DatePickerFragment");
+
             }
         });
 
+        next1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             viewPager = getActivity().findViewById(R.id.viewpager);
+             viewPager.setCurrentItem(1);
+            }
+        });
 
         return rootView;
     }
@@ -47,12 +72,10 @@ public class PersonalFragment extends Fragment {
 
     DatePickerDialog.OnDateSetListener onDate = new DatePickerDialog.OnDateSetListener() {
         @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
+        public void onDateSet(DatePicker view, int year, int monthOfYear,int dayOfMonth) {
 
             input_date =  getActivity().findViewById(R.id.input_date);
-            input_date.setText(String.valueOf(year) + "-" + String.valueOf(monthOfYear)
-                    + "-" + String.valueOf(dayOfMonth));
+            input_date.setText(String.valueOf(year) + "-" + String.valueOf(monthOfYear)+ "-" + String.valueOf(dayOfMonth));
         }
     };
 
